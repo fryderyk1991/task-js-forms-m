b40feca412988d13499a3df7b1d7096d298b371d
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-08-31 22:13:40 +0200

    add function responsible for add content from csv file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,3 @@
-// const txt = `"1","Ogrodzieniec","Zamek Ogrodzieniec – ruiny zamku leżącego na Jurze Krakowsko-Częstochowskiej, wybudowanego w systemie tzw. Orlich Gniazd, we wsi Podzamcze w województwie śląskim, w powiecie zawierciańskim, około 2 km na wschód od Ogrodzieńca. Zamek został wybudowany w XIV – XV w. przez ród Włodków Sulimczyków.","99PLN","50PLN"
-// "2","Ojców","wieś w województwie małopolskim, w powiecie krakowskim, w gminie Skała, na terenie Wyżyny Krakowsko-Częstochowskiej, w Dolinie Prądnika, na Szlaku Orlich Gniazd. W Królestwie Polskim istniała gmina Ojców. W latach 1975–1998 miejscowość położona była w województwie krakowskim. W latach 1928–1966 Ojców miał status uzdrowiska posiadającego charakter użyteczności publicznej.","40PLN","15PLN`;
-
-// console.log( txt.split( /[\r\n]+/gm) );
-
 const uploaderInput = document.querySelector(".uploader__input");
 uploaderInput.addEventListener("change", uploadTravels);
 
@@ -12,28 +7,80 @@ function uploadTravels(e) {
     const reader = new FileReader();
     reader.onload = function (event) {
       const data = event.target.result;
-      convertCsvToArray(data)
+      convertCsvToArray(data);
     };
     reader.readAsText(file);
   }
 }
 
 function convertCsvToArray(stringValue) {
-  const newStringValue = stringValue.split('\n');
-  
+  const newStringValue = stringValue.split("\n");
+
   const objectsArray = [];
 
   for (const string of newStringValue) {
-    const values = string.replace(/"/g, '').trim().split(',');
+    const values = string.replace(/"/g, "").trim().split(",");
 
     const obj = {
-      id: values[0],
-      name: values[1],
-      description: values.slice(2, -2),
-      adultPrice: values[values.length - 2],
-      childPrice: values[values.length - 1],
-    }
-    objectsArray.push(obj)
+      id: +values[0],
+      title: values[1],
+      description: values.slice(2, -2).join(''), 
+      adultPrice: +values[values.length - 2],
+      childPrice: +values[values.length - 1],
+    };
+    objectsArray.push(obj);
   }
+  addTravelsToDOM(objectsArray);
 }
+
+function addTravelsToDOM(travelsData) {
+    const travels = travelsData;
+    const listTravels = document.querySelector('.excursions');
+    travels.forEach(function(travel) {
+      listTravels.innerHTML += `
+      <li data-id="${travel.id}" class="excursions__item excursions__item--prototype">
+      <header class="excursions__header">
+        <h2 class="excursions__title">${travel.title}</h2>
+        <p class="excursions__description">${travel.description}</p>
+      </header>
+      <form class="excursions__form">
+        <div class="excursions__field">
+          <label class="excursions__field-name">
+            Dorosły: <span class="excursions__price">${travel.adultPrice}</span>PLN x
+            <input class="excursions__field-input" name="adults" />
+          </label>
+        </div>
+        <div class="excursions__field">
+          <label class="excursions__field-name">
+            Dziecko: <span class="excursions__price">${travel.childPrice}</span>PLN x
+            <input class="excursions__field-input" name="children" />
+          </label>
+        </div>
+        <div class="excursions__field excursions__field--submit">
+          <input
+            class="excursions__field-input excursions__field-input--submit"
+            value="dodaj do zamówienia"
+            type="submit"
+          />
+        </div>
+      </form>
+    </li>
+      `
+    })
+    getAccesToForm(listTravels)
+}
+
+
+function getAccesToForm (listTravels) {
+    const list = listTravels;
+    const listChildren = [...list.children];
+    console.log(listChildren)
+
+  listChildren.forEach(function(li) {
+    const formEl = li.querySelector('form');
+    
+    return formEl;
+  })
+}
+
 
